576e4458c862fdcf9fe92c25b148335b09e6b7a5
Terenter <too.mek@wp.pl>
2017-05-13 15:47:53 +0200

    Otwieranie / usuwanie.

--- IO2P/nancyTest.cs
+++ IO2P/nancyTest.cs
@@ -1,4 +1,5 @@
 ﻿using Nancy;
+using System.IO;
 
 namespace IO2P
 {
@@ -18,6 +19,28 @@ namespace IO2P
             Get["/listfiles"] = _ => View["front/myfiles.html"];
             Get["/upload.html"] = _ => View["front/upload.html"];
             Get["/newfile"] = _ => View["front/upload.html"];
+            // Get["/getfile"] = _ =>
+            // {
+            //     string contentType = this.Request.Query["contentType"];
+            //     byte[] file = new resourceViewer().handleRequest(this.Request);
+            //     return Response.FromByteArray(file, contentType);
+            // };
+            Post["/getfile"] = _ =>
+            {
+                string contentType = this.Request.Form.contentType;
+                byte[] file = new resourceViewer().handleRequest(this.Request);
+                return Response.FromByteArray(file, contentType);
+            };
+            Post["/removefile"] = _ =>
+            {
+               new resourceDeleter().handleRequest(this.Request);
+               return true;
+            };
+            // Get["/removefile"] = _ =>
+            // {
+            //     new resourceDeleter().handleRequest(this.Request);
+            //    return true;
+            // };
             Post["/upload.html"] = _ => new resourceAdder().handlePost(this.Request);
             Post["/newfile"] = _ => new resourceAdder().handlePost(this.Request);
             Post["/newfile"] = _ =>
