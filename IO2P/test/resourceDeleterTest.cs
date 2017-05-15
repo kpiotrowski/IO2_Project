9367d5d9a6253d8ac6a3fb03b4e1581ffb8c195c
M-Lewinski <lewin.michal@gmail.com>
2017-05-15 10:05:07 +0200

    more tests

--- IO2P/test/resourceDeleterTest.cs
+++ IO2P/test/resourceDeleterTest.cs
@@ -14,24 +14,71 @@ namespace IO2P.test
     public class resourceDeleterTest
     {
         private resourceDeleter res;
+        private fileEntry file;
+        private Mock<IMongoDatabase> dbMock = new Mock<IMongoDatabase>();
+        private Mock<IMongoCollection<fileEntry>> dbCollection = new Mock<IMongoCollection<fileEntry>>();
+
         [SetUp]
         public void setUpTest()
         {
-            Mock<IMongoDatabase> dbMock = new Mock<IMongoDatabase>();
-            Mock<IMongoCollection<fileEntry>> dbCollection = new Mock<IMongoCollection<fileEntry>>();
-            fileEntry file = new fileEntry("plik.jpg", "folder", "cat", "audio");
-            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(),null,default(System.Threading.CancellationToken))).Returns(file);
-            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
-            DbaseMongo.Instance.db = dbMock.Object;
             this.res = new resourceDeleter();
         }
 
-        [Test]
-        public void findAndDeleteResourceLocation_Correct()
+        [TestCase("59185507cd9412107c226b99", "plik.jpg", "folder", "cat", "audio")]
+        public void findAndDeleteResourceLocation_JPG(string fieldId, string name, string loc, string cat, string type)
         {
-            string loc = this.res.findAndDeleteResourceLocation("59185507cd9412107c226b99");
-            StringAssert.AreEqualIgnoringCase("folder/plik.jpg" , loc);
+            this.file = new fileEntry(name, loc, cat, type);
+            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(), null, default(System.Threading.CancellationToken))).Returns(file);
+            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
+            DbaseMongo.Instance.db = dbMock.Object;
+            string result = this.res.findAndDeleteResourceLocation(fieldId);
+            StringAssert.AreEqualIgnoringCase(loc+"/"+name , result);
         }
+
+        [TestCase("59185507cd9412107c226b99", "test.png", "localization", "newKat", "image")]
+        public void findAndDeleteResourceLocation_PNG(string fieldId, string name, string loc, string cat, string type)
+        {
+            this.file = new fileEntry(name, loc, cat, type);
+            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(), null, default(System.Threading.CancellationToken))).Returns(file);
+            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
+            DbaseMongo.Instance.db = dbMock.Object;
+            string result = this.res.findAndDeleteResourceLocation(fieldId);
+            StringAssert.AreEqualIgnoringCase(loc + "/" + name, result);
+        }
+
+        [TestCase("59185507cd9412107c226b99", "mongo.mp3", "testowanie/nowejLokalizacji", "Empty", "video")]
+        public void findAndDeleteResourceLocation_MP3(string fieldId, string name, string loc, string cat, string type)
+        {
+            this.file = new fileEntry(name, loc, cat, type);
+            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(), null, default(System.Threading.CancellationToken))).Returns(file);
+            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
+            DbaseMongo.Instance.db = dbMock.Object;
+            string result = this.res.findAndDeleteResourceLocation(fieldId);
+            StringAssert.AreEqualIgnoringCase(loc + "/" + name, result);
+        }
+
+        [TestCase("59185507cd9412107c226b99", "mock.avi", "folder", "superKategoria", "video")]
+        public void findAndDeleteResourceLocation_AVI(string fieldId, string name, string loc, string cat, string type)
+        {
+            this.file = new fileEntry(name, loc, cat, type);
+            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(), null, default(System.Threading.CancellationToken))).Returns(file);
+            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
+            DbaseMongo.Instance.db = dbMock.Object;
+            string result = this.res.findAndDeleteResourceLocation(fieldId);
+            StringAssert.AreEqualIgnoringCase(loc + "/" + name, result);
+        }
+
+        [TestCase("lkjlj", "typAudio.bmp", "testu", "fjdslkfjsdklfjsdlkfj", "image")]
+        public void findAndDeleteResourceLocation_BMP(string fieldId, string name, string loc, string cat, string type)
+        {
+            this.file = new fileEntry(name, loc, cat, type);
+            dbCollection.Setup(col => col.FindOneAndDelete<fileEntry>(It.IsAny<FilterDefinition<fileEntry>>(), null, default(System.Threading.CancellationToken))).Returns(file);
+            dbMock.Setup(db => db.GetCollection<fileEntry>(DbaseMongo.DefaultCollection, null)).Returns(dbCollection.Object);
+            DbaseMongo.Instance.db = dbMock.Object;
+            string result = this.res.findAndDeleteResourceLocation(fieldId);
+            StringAssert.AreEqualIgnoringCase(null, result);
+        }
+
 
         [TestCase("")]
         [TestCase(null)]
